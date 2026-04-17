672e4dd741d9fabb5658df770fd33b83f9c5588d
Fmfg100 <143935371+Fmfg100@users.noreply.github.com>
2026-04-17 20:46:46 +0200

    Fix text input and replace print with st.write

--- MoodJournalAnalyzer.py
+++ MoodJournalAnalyzer.py
@@ -4,18 +4,18 @@ import streamlit as st
 # Draw a title and some text to the app:
 title = st.title("Mood Journal Analyzer!")
 
-text = st.text_input("Please enter a sentance about your day: ", placeholder="How was your day?")
+text = st.text_input("Please enter a sentence about your day: ", placeholder="How was your day?")
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity
 
 if sentiment > 0.1:
-    print("Positive")
+    st.write("Positive")
 elif sentiment >= -0.1:
-    print("Neutral")
+    st.write("Neutral")
 else:
-    print("Negative")
+    st.write("Negative")
 if sentiment > 0.1:
-    print(f"your day is {blob.sentiment.polarity * 100:.1f}%" + " Positive")
+    st.write(f"your day is {blob.sentiment.polarity * 100:.1f}%" + " Positive")
 if sentiment < -0.1:
     score = sentiment * -100
-    print(f"your day is {score:.1f}% Negative")
+    st.write(f"your day is {score:.1f}% Negative")
