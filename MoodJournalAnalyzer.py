16895324b62627998b779f64ef47b253aa094029
yaseenmind11-art <yaseen.mind11@gmail.com>
2026-04-23 19:11:36 +0200

    Update MoodJournalAnalyzer.py

--- MoodJournalAnalyzer.py
+++ MoodJournalAnalyzer.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 import streamlit as st
 st.set_page_config(
-    page_title="Mood Journal Analyzer" ,
+    page_title="Mood Journal Analyzer!" ,
     page_icon="📖" ,
     initial_sidebar_state="collapsed"
 )
